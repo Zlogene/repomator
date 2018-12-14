44d76bf25e8b68f164682045c4b7eb4b18176634
Mikle Kolyada <zlogene@users.noreply.github.com>
2018-12-14 21:05:23 +0300

    repomator.py: split ekeyword and repoman

--- repomator.py
+++ repomator.py
@@ -27,28 +27,41 @@ check_existence("/usr/bin/ekeyword")
 parser = argparse.ArgumentParser(description='Repomator script v1.0')
 parser.add_argument('-a', '--arch', help='specify architecture', required=True)
 parser.add_argument('-b', '--bug', type=int, help='specify bug number', required=True)
-parser.add_argument('-e', '--email', help='specify email addrress')
+parser.add_argument('-e', '--email', help='specify email address')
 parser.add_argument('-l', '--list', help='specify packages list', required=True)
 parser.add_argument('-r', '--repo', help='specify repo path', required=True)
 
 args = parser.parse_args()
 
+packages = []
+
 with open(args.list, 'r') as f:
+
     for line in f:
 
         package_category = re.search(r'((?<==)\w+.\w+|\w+.\w+)', line).group(0)
         package_name = re.search(r'(?<=/).*(?=-\d)|(\w+_\w+)', line).group(0)
         package_version = re.search(r'(?<=-)\d.*?(?=\s)', line).group(0)
 
-        os.chdir(os.path.join(args.repo, package_category, package_name))
+        packages.append({
+            "category": package_category,
+            "name": package_name,
+            "version": package_version
+        })
 
-        os.system("/usr/bin/ekeyword {} {}-{}.ebuild".format(args.arch, package_name, package_version))
-        
-        if args.arch[:1] is not "~":
+for package in packages:
+
+        os.chdir(os.path.join(args.repo, package["category"], package["name"]))
+        os.system("/usr/bin/ekeyword {} {}-{}.ebuild".format(args.arch, package["name"], package["version"]))
+
+for package in packages:
+
+    os.chdir(os.path.join(args.repo, package["category"], package["name"]))
+    if args.arch[:1] is not "~":
             os.system("/usr/bin/repoman ci -m \"{}/{}: {} stable wrt bug #{}\""
-                      .format(package_category, package_name, args.arch, args.bug))
-        else:
+                      .format(package["category"], package["name"], args.arch, args.bug))
+    else:
             os.system("/usr/bin/repoman ci -m \"{}/{}: Add {} keyword wrt bug #{}\""
-                      .format(package_category, package_name, args.arch, args.bug))
+                      .format(package["category"], package["name"], args.arch, args.bug))
 
 bugtracker(args.arch, args.bug)
