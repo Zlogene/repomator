e9a43d89e3ae99d93570728d0707b90b664e82ed
Mikle Kolyada <zlogene@gentoo.org>
2019-01-13 17:41:13 +0300

    restructure project and add setup.py
    Signed-off-by: Mikle Kolyada <zlogene@gentoo.org>

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='repomator',
+    version='2.0',
+    url='https://github.com/Zlogene/repomator',
+    license='Apache-2.0',
+    packages=find_packages(),
+    scripts=["bin/repomator"],
+    author='Mikle Kolyada',
+    author_email='zlogene@gentoo.org',
+    description='Gentoo Gnu/Linux branches tooling',
+
+    data_files=[('/etc/repomator', ['data/repomator.yaml'])],
+
+    install_requires=[
+        'beautifulsoup4 >= 4.5.1',
+        'requests >= 2.18.4',
+        'pyyaml >= 3.13'
+    ]
+)
